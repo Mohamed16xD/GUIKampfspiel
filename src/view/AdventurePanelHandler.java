ab8bbe0b2067271274ea93539e50d3b6faabc8de
mlynkacp <mlynkacp@kkg-do.eu>
2020-09-30 13:14:48 +0200

    Panels angefangen

--- src/view/AdventurePanelHandler.java
+++ src/view/AdventurePanelHandler.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class AdventurePanelHandler {
     private JPanel panel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
     private MainView mainView;
 
     public AdventurePanelHandler(MainView mainView) {
--- src/view/StartPanelHandler.java
+++ src/view/StartPanelHandler.java
@@ -6,6 +6,12 @@ import java.awt.event.ActionListener;
 
 public class StartPanelHandler {
     private JPanel panel;
+    private JTextField textField1;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JLabel nameLabel;
+    private JLabel submarineLabel;
+    private JLabel boatLabel;
     private MainView mainView;
 
     public StartPanelHandler(MainView mainView) {
@@ -18,6 +24,9 @@ public class StartPanelHandler {
     }
 
     private void createButtons(){
+        nameLabel.setName("Name");
+        submarineLabel.setName("Submarine");
+        boatLabel.setName("Boat");
 
     }
 }
