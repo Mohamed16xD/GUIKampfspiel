6b6cf1f7c9dd637f68ffce7bc52faab9096e6064
elmamoha <elmamoha@kkg-do.eu>
2020-09-30 13:12:55 +0200

    MainController

--- src/control/MainController.java
+++ src/control/MainController.java
@@ -1,4 +1,37 @@
 package control;
 
+
 public class MainController {
+
+    public MainController(){
+
+    }
+
+    public void createNewWaterCraft(String name, boolean boat){
+
+    }
+
+    public String[] getWatercraftInfos(){
+        return null;
+    }
+
+    public String createNewMonster(int level){
+        return null;
+    }
+
+    public String[] getMonsterInfos(){
+
+        return null;
+
+    }
+    public String fight(){
+        return null;
+
+    }
+
+    public String flee(){
+        return null;
+
+    }
+
 }
