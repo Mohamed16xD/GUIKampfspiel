b219f74148aff4b31159394539ce9cff1f35966e
aos <aos@kkg-do.eu>
2020-10-01 09:33:36 +0200

    Kampfpanels aktualisiert.

--- src/view/FightPanelHandler.java
+++ src/view/FightPanelHandler.java
@@ -3,9 +3,23 @@ package view;
 import control.MainController;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FightPanelHandler {
     private JPanel panel;
+    private JTextArea fightInfo;
+    private JButton fightButton;
+    private JButton fleeButton;
+    private JLabel monsterName;
+    private JLabel monsterLevel;
+    private JLabel monsterPower;
+    private JLabel monsterHealth;
+    private JLabel playerName;
+    private JLabel playerLevel;
+    private JLabel playerPower;
+    private JLabel playerHealth;
+    private JButton backToAdventure;
     private MainView mainView;
     private MainController mainController;
 
@@ -13,6 +27,9 @@ public class FightPanelHandler {
         createButtons();
         this.mainController = mainController;
         this.mainView = mainView;
+
+        updateGUI();
+
     }
 
     public JPanel getPanel(){
@@ -20,6 +37,47 @@ public class FightPanelHandler {
     }
 
     private void createButtons(){
+        fightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                fight();
+            }
+        });
+        fleeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                flee();
+            }
+        });
+        backToAdventure.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
 
+            }
+        });
+    }
+
+    private void fight(){
+        updateGUI();
+    }
+
+    private void flee(){
+        updateGUI();
+    }
+
+    private void updateGUI(){
+        endPanel();
+    }
+
+    private void startPanel(){
+        fightButton.setEnabled(true);
+        fleeButton.setEnabled(true);
+        backToAdventure.setVisible(false);
+    }
+
+    private void endPanel(){
+        fightButton.setEnabled(false);
+        fleeButton.setEnabled(false);
+        backToAdventure.setVisible(true);
     }
 }
