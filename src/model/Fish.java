f03345766993d795b1901d5ecd57559c8904f362
EmreArik0 <arikemre@kkg-do.eu>
2020-10-02 14:15:00 +0200

    neue Klasse Fish

---
+++ src/model/Fish.java
@@ -0,0 +1,27 @@
+package model;
+
+public abstract class Fish extends OceanMonster{
+    public Fish(String name, int level ) {
+        super(name);
+        int random = (int) (Math.random() * 2 + (level - 1));
+        fightPowerAbove = 1 * random;
+        random = (int) (Math.random() * 2 + (level - 1));
+        fightPowerBelow = 2 * random;
+        random = (int) (Math.random() * 2 + (level - 1));
+        defense = (int) 1.5 * random;
+        random = (int) (Math.random() * 2 + (level - 1));
+        followPowerAbove = (int) 0.5 * random;
+        random = (int) (Math.random() * 2 + (level - 1));
+        followPowerBelow = (int) 1.5 * random;
+    }
+        public int getFightPower(Boolean isBoat){
+            if(isBoat == true){
+                return fightPowerAbove;
+            }else{
+                return fightPowerBelow*2;
+            }
+        }
+
+    }
+
+
