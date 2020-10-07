edc88e594c4e80add200e1d090d585beb0ab4d67
ucar1 <oemeucar@kkg-do.eu>
2020-10-07 21:33:26 +0200

    Watercraft JavaDoc

--- src/model/Watercraft.java
+++ src/model/Watercraft.java
@@ -5,6 +5,12 @@ public abstract class Watercraft extends OceanObject {
     private int currentXP;
     protected int fleePower;
 
+    /**
+     * Ein Objekt der Klasse Watercraft wird erzeugt,indem der Konstruktor der Oberklasse OcaenObjekt
+     * aufgerufen und der übergebene Parameter n weitergereicht wird.
+     * currentXP bekommt den Wert 0, fleePower bekommt den Wert 2 und level bekommt den Wert 1.
+     * @param n
+     */
     public Watercraft(String n){
         super(n);
         currentXP = 0;
@@ -20,14 +26,21 @@ public abstract class Watercraft extends OceanObject {
         return fleePower;
     }
 
+    /**
+     *currentXP wird mit amount addiert.
+     * Solange currentXP nicht den Wert 100 überschreitet wird die abstrackte Methode gainLevel() aufgerufen.
+     * Außerdem wird currentXP mit 1 multipliziert und durch 10 geteilt.
+     * @param amount
+     */
     public void gainXP(int amount){
         currentXP = amount + currentXP;
-        if (currentXP > 10){
-
+        if (currentXP < 100) {
+            currentXP = 1*currentXP/10;
             gainLevel();
-            currentXP = currentXP/10;
 
         }
+
+
 
 
         //TODO Weitere XP-Grenzen hinzufügen und darauf achten, dass man nicht zu schnell in der Erfahrung steigt.
